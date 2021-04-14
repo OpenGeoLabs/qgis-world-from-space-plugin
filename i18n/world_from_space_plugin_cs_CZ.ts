554c06c73158229dd3baaff844904a1b86008c3a
ruz76 <jan.ruzicka@cybele.space>
2021-04-14 03:51:03 +0200

    Translations.

--- i18n/world_from_space_plugin_cs_CZ.ts
+++ i18n/world_from_space_plugin_cs_CZ.ts
@@ -13,6 +13,21 @@
         <source>API Key</source>
         <translation>API klíč</translation>
     </message>
+    <message>
+        <location filename="../settings_base.ui" line="65"/>
+        <source>Directory where save layers</source>
+        <translation>Adresář kam ukládat data</translation>
+    </message>
+    <message>
+        <location filename="../settings_base.ui" line="91"/>
+        <source>Browse</source>
+        <translation>Procházet</translation>
+    </message>
+    <message>
+        <location filename="../settings_base.ui" line="94"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
 </context>
 <context>
     <name>Form</name>
@@ -24,41 +39,76 @@
     <message>
         <location filename="../world_from_space_widget_base.ui" line="32"/>
         <source>Register polygons</source>
-        <translation>Registrovat polygony</translation>
+        <translation type="obsolete">Registrovat polygony</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget_base.ui" line="58"/>
+        <location filename="../world_from_space_widget_base.ui" line="35"/>
         <source>Get data</source>
         <translation>Získat data</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget_base.ui" line="84"/>
+        <location filename="../world_from_space_widget_base.ui" line="61"/>
         <source>dd.MM.yyyy</source>
         <translation>dd.MM.yyyy</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget_base.ui" line="107"/>
+        <location filename="../world_from_space_widget_base.ui" line="84"/>
         <source>Settings</source>
         <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget_base.ui" line="110"/>
+        <source>Save Layers</source>
+        <translation>Uložit vybrané vrstvy</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget_base.ui" line="126"/>
+        <source>Start date:</source>
+        <translation>Datum od:</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget_base.ui" line="139"/>
+        <source>End date:</source>
+        <translation>Datum do:</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget_base.ui" line="152"/>
+        <source>Type of index:</source>
+        <translation>Typ indexu:</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget_base.ui" line="165"/>
+        <source>Form of result:</source>
+        <translation>Varianta výstupu:</translation>
     </message>
 </context>
 <context>
     <name>World from Space</name>
     <message>
-        <location filename="../world_from_space_widget.py" line="254"/>
+        <location filename="../world_from_space_widget.py" line="363"/>
         <source>Error</source>
         <translation>Chyba</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget.py" line="254"/>
+        <location filename="../world_from_space_widget.py" line="300"/>
         <source>The response does not contain valid data to show.</source>
         <translation>Odpověď serveru neobsahuje žádná data, která by šlo zobrazit.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="185"/>
+        <source>Can not register selected polygons. Check if the polygon in single geometry.</source>
+        <translation>Nemohu registrovat vybrané polygony. ZKontrolujte zda se jedná o jednoduchou geometrii.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="363"/>
+        <source>Selected raster layers were saved localy.</source>
+        <translation>Vybrané rastrové vrstvy byly uloženy lokálně.</translation>
     </message>
 </context>
 <context>
     <name>WorldFromSpace</name>
     <message>
-        <location filename="../world_from_space.py" line="183"/>
+        <location filename="../world_from_space.py" line="185"/>
         <source>&amp;WorldFromSpace</source>
         <translation>&amp;WorldFromSpace</translation>
     </message>
@@ -71,19 +121,39 @@
 <context>
     <name>WorldFromSpaceWidget</name>
     <message>
-        <location filename="../world_from_space_widget.py" line="131"/>
+        <location filename="../world_from_space_widget.py" line="349"/>
         <source>ERROR</source>
         <translation>CHYBA</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget.py" line="126"/>
+        <location filename="../world_from_space_widget.py" line="134"/>
         <source>You have to select one layer.</source>
         <translation>Musíte vybrat jednu vrstvu.</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget.py" line="131"/>
+        <location filename="../world_from_space_widget.py" line="142"/>
         <source>You have to select at least one feature.</source>
         <translation>Musíte vybrat minimálně jeden polygon.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="198"/>
+        <source>File for storing registered polygons is not available</source>
+        <translation>Soubor pro ukládání registrovaných polygonů není k dispozici.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="137"/>
+        <source>You have to select vector layer.</source>
+        <translation>Musíte vybrat vektorovou vrstvu.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="349"/>
+        <source>You have to select at least one layer.</source>
+        <translation>Musíte vybrat minimálně jednu vrstvu.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="353"/>
+        <source>Selected layer is not raster. Skipping</source>
+        <translation>Vybraná vrstva není rastrová. Přeskakuji.</translation>
     </message>
 </context>
 </TS>
--- i18n/world_from_space_plugin_en_US.ts
+++ i18n/world_from_space_plugin_en_US.ts
@@ -13,6 +13,21 @@
         <source>API Key</source>
         <translation>API Key</translation>
     </message>
+    <message>
+        <location filename="../settings_base.ui" line="65"/>
+        <source>Directory where save layers</source>
+        <translation>Directory where save layers</translation>
+    </message>
+    <message>
+        <location filename="../settings_base.ui" line="91"/>
+        <source>Browse</source>
+        <translation>Browse</translation>
+    </message>
+    <message>
+        <location filename="../settings_base.ui" line="94"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
 </context>
 <context>
     <name>Form</name>
@@ -24,41 +39,76 @@
     <message>
         <location filename="../world_from_space_widget_base.ui" line="32"/>
         <source>Register polygons</source>
-        <translation>Register polygons</translation>
+        <translation type="obsolete">Register polygons</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget_base.ui" line="58"/>
+        <location filename="../world_from_space_widget_base.ui" line="35"/>
         <source>Get data</source>
         <translation>Get data</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget_base.ui" line="84"/>
+        <location filename="../world_from_space_widget_base.ui" line="61"/>
         <source>dd.MM.yyyy</source>
         <translation>dd.MM.yyyy</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget_base.ui" line="107"/>
+        <location filename="../world_from_space_widget_base.ui" line="84"/>
         <source>Settings</source>
         <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget_base.ui" line="110"/>
+        <source>Save Layers</source>
+        <translation>Save selected layers</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget_base.ui" line="126"/>
+        <source>Start date:</source>
+        <translation>Start date:</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget_base.ui" line="139"/>
+        <source>End date:</source>
+        <translation>End date:</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget_base.ui" line="152"/>
+        <source>Type of index:</source>
+        <translation>Type of index:</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget_base.ui" line="165"/>
+        <source>Form of result:</source>
+        <translation>Form of result:</translation>
     </message>
 </context>
 <context>
     <name>World from Space</name>
     <message>
-        <location filename="../world_from_space_widget.py" line="254"/>
+        <location filename="../world_from_space_widget.py" line="363"/>
         <source>Error</source>
         <translation>Error</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget.py" line="254"/>
+        <location filename="../world_from_space_widget.py" line="300"/>
         <source>The response does not contain valid data to show.</source>
         <translation>The response does not contain valid data to show.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="185"/>
+        <source>Can not register selected polygons. Check if the polygon in single geometry.</source>
+        <translation>Can not register selected polygons. Check if the polygon in single geometry.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="363"/>
+        <source>Selected raster layers were saved localy.</source>
+        <translation>Can not register selected polygons. Check if the polygon in single geometry.</translation>
     </message>
 </context>
 <context>
     <name>WorldFromSpace</name>
     <message>
-        <location filename="../world_from_space.py" line="183"/>
+        <location filename="../world_from_space.py" line="185"/>
         <source>&amp;WorldFromSpace</source>
         <translation>&amp;WorldFromSpace</translation>
     </message>
@@ -71,19 +121,39 @@
 <context>
     <name>WorldFromSpaceWidget</name>
     <message>
-        <location filename="../world_from_space_widget.py" line="131"/>
+        <location filename="../world_from_space_widget.py" line="349"/>
         <source>ERROR</source>
         <translation>ERROR</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget.py" line="126"/>
+        <location filename="../world_from_space_widget.py" line="134"/>
         <source>You have to select one layer.</source>
         <translation>You have to select one layer.</translation>
     </message>
     <message>
-        <location filename="../world_from_space_widget.py" line="131"/>
+        <location filename="../world_from_space_widget.py" line="142"/>
         <source>You have to select at least one feature.</source>
         <translation>You have to select at least one feature.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="198"/>
+        <source>File for storing registered polygons is not available</source>
+        <translation>File for storing registered polygons is not available.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="137"/>
+        <source>You have to select vector layer.</source>
+        <translation>You have to select vector layer.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="349"/>
+        <source>You have to select at least one layer.</source>
+        <translation>You have to select at least one layer.</translation>
+    </message>
+    <message>
+        <location filename="../world_from_space_widget.py" line="353"/>
+        <source>Selected layer is not raster. Skipping</source>
+        <translation>Selected layer is not raster. Skipping.</translation>
     </message>
 </context>
 </TS>
